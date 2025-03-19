dc5030d3d0646233cf0d80ad4fd090fb55fb3b53
Arsh Sharma <arshsharma461@gmail.com>
2025-03-19 13:05:22 +0530

    player page ui completed
    Signed-off-by: Arsh Sharma <arshsharma461@gmail.com>

--- src/pages/Players.tsx
+++ src/pages/Players.tsx
@@ -9,34 +9,59 @@ import {
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useForm } from "@mantine/form";
+import { useState } from "react";
+import { IconTrash } from "@tabler/icons-react";
+
+interface Player {
+  id: number;
+  name: string;
+  gamesPlayed: number;
+}
+
+let id = 4;
 
 export default function Players() {
   const [opened, { open, close }] = useDisclosure(false);
   const form = useForm({
     mode: "uncontrolled",
     initialValues: {
+      id: id,
       name: "",
+      gamesPlayed: 0,
     },
   });
-  const players = [
-    { name: "Arsh", gamesPlayed: 2 },
-    { name: "Nitish", gamesPlayed: 3 },
-    { name: "Amit", gamesPlayed: 7 },
-    { name: "Saloni", gamesPlayed: 5 },
-    { name: "Abhinav", gamesPlayed: 13 },
-  ];
+
+  const [players, setPlayers] = useState<Player[]>([
+    { id: 1, name: "Arsh", gamesPlayed: 2 },
+    { id: 2, name: "Nitish", gamesPlayed: 3 },
+    { id: 3, name: "Amit", gamesPlayed: 7 },
+  ]);
 
   const rows = players.map((player) => (
     <Table.Tr key={player.name}>
       <Table.Td>{player.name}</Table.Td>
       <Table.Td>{player.gamesPlayed}</Table.Td>
+      <Table.Td>
+        <IconTrash
+          size={20}
+          stroke={1.5}
+          color="var(--mantine-color-blue-filled)"
+          onClick={() => setPlayers(players.filter((p) => p.id !== player.id))}
+        />
+      </Table.Td>
     </Table.Tr>
   ));
+
+  function handleOnSubmit(values: Player) {
+    setPlayers([...players, { id: id++, name: values.name, gamesPlayed: 0 }]);
+    console.log(id);
+    form.reset();
+  }
 
   return (
     <>
       <Modal opened={opened} onClose={close} title="Add a Player">
-        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <form onSubmit={form.onSubmit((values) => handleOnSubmit(values))}>
           <TextInput
             withAsterisk
             label="Name"
@@ -45,7 +70,9 @@ export default function Players() {
             {...form.getInputProps("name")}
           />
           <Group justify="flex-end" mt="md">
-            <Button type="submit">Submit</Button>
+            <Button type="submit" onClick={close}>
+              Submit
+            </Button>
           </Group>
         </form>
       </Modal>
